4ed74c1fd20be9dbdaed0c67b6765dbbeef357ca
csdoge <bowenlu.cloud@gmail.com>
2021-10-27 00:06:03 +0800

    Update KaiserDemo.java

--- encrypt-decrypt/src/main/java/com/atguigu/kaiser/KaiserDemo.java
+++ encrypt-decrypt/src/main/java/com/atguigu/kaiser/KaiserDemo.java
@@ -5,6 +5,25 @@ public class KaiserDemo {
         String input = "Hello World";
         // 右移三位
         int key = 3;
+        String ciphertext = encrypted(input, key);
+        System.out.println("密文是： " + ciphertext);
+        String plaintext = decrypted(ciphertext, key);
+        System.out.println("明文是： " + plaintext);
+    }
+
+    private static String decrypted(String input, int key) {
+        char[] chars = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            int b = c;
+            b -= key;
+            char newChar = (char) b;
+            sb.append(newChar);
+        }
+        return sb.toString();
+    }
+
+    private static String encrypted(String input, int key) {
         char[] chars = input.toCharArray();
         StringBuilder sb = new StringBuilder();
         for (char c : chars) {
@@ -13,6 +32,6 @@ public class KaiserDemo {
             char newChar = (char) b;
             sb.append(newChar);
         }
-        System.out.println(sb.toString());
+        return sb.toString();
     }
 }
