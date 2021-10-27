15d6aa307edd5093fb9716f7ee55409861f50b07
csdoge <bowenlu.cloud@gmail.com>
2021-10-27 23:51:17 +0800

    Update DesAesDemo.java

--- encrypt-decrypt/src/main/java/com/atguigu/desaes/DesAesDemo.java
+++ encrypt-decrypt/src/main/java/com/atguigu/desaes/DesAesDemo.java
@@ -1,8 +1,13 @@
 package com.atguigu.desaes;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class DesAesDemo {
@@ -16,7 +21,30 @@ public class DesAesDemo {
         // 加密类型
         String type = "DES";
         // 创建加密对象
-        Cipher cipher = Cipher.getInstance("DES");
+        String ciphertext = encryptedDES(input, key, type);
+        System.out.println(ciphertext);
+//        for (byte aByte : bytes) {
+//            System.out.println(aByte);
+//        }
+//        System.out.println(new String(bytes));
+        String plaintext = decryptedDES(ciphertext, key, type);
+        System.out.println(plaintext);
+    }
+
+    private static String decryptedDES(String encryptedMsg, String key, String type) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(type);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), type);
+        // 第一个参数表示解密
+        // 第二个参数表示解密规则
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] bytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMsg));
+        // 需要使用new String(bytes)
+        // 不能使用bytes.toString()
+        return new String(bytes);
+    }
+
+    private static String encryptedDES(String input, String key, String type) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(type);
         // 第一个参数表示key的字节
         // 第二个参数表示加密的类型
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), type);
@@ -29,10 +57,7 @@ public class DesAesDemo {
         // 创建base64对象
 
         String encode = Base64.getEncoder().encodeToString(bytes);
-        System.out.println(encode);
-//        for (byte aByte : bytes) {
-//            System.out.println(aByte);
-//        }
-//        System.out.println(new String(bytes));
+        return encode;
+
     }
 }
